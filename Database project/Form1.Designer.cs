505e36f06a4aa01f4a232aeb3df2e5cbb2108a4f
nour <nouurmohaamed@icloud.com>
2026-05-01 19:47:19 +0300

    Add project files.

---
+++ Database project/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Database_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nxtFormBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nxtFormBtn
+            // 
+            this.nxtFormBtn.Location = new System.Drawing.Point(672, 404);
+            this.nxtFormBtn.Name = "nxtFormBtn";
+            this.nxtFormBtn.Size = new System.Drawing.Size(116, 34);
+            this.nxtFormBtn.TabIndex = 0;
+            this.nxtFormBtn.Text = "Next";
+            this.nxtFormBtn.UseVisualStyleBackColor = true;
+            this.nxtFormBtn.Click += new System.EventHandler(this.nxtFormBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nxtFormBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nxtFormBtn;
+    }
+}
+
---
+++ Database project/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; //This library contains the "tools" needed to talk to SQL Server.
+
+namespace Database_project
+{
+    public partial class Form1 : Form //this is the class for the main form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=localhost; Initial Catalog=PharmacyMgmtDB; Integrated Security=True; TrustServerCertificate=True");
+        public Form1() // constructor that runs the moment the window is created 
+        {
+            InitializeComponent();
+           
+        }
+
+        private void nxtFormBtn_Click(object sender, EventArgs e)
+        {
+        Form2 newform = new Form2();
+            newform.Show();
+            this.Hide();
+        }
+    }
+}
